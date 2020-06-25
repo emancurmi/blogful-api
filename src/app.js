d318d62db7200bf1dc9e1d209039b5fc3438b46e
Eman Curmi <emancurmi@gmail.com>
2020-06-25 02:47:14 -0400

    updates

--- src/app.js
+++ src/app.js
@@ -4,49 +4,50 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
+
 const app = express()
 
-const cardRouter = require('./card/card-router')
-const listRouter = require('./list/list-router')
-
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-    : 'common';
-
-const { v4: uuid } = require('uuid');
-
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
+app.use(helmet())
 
-app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    // move to the next middleware
-    next()
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
 })
 
-app.use(cardRouter)
-app.use(listRouter)
-
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-
-    res.status(500).json(response)
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if (!article) {
+                return res.status(404).json({
+                    error: { message: `Article doesn't exist` }
+                })
+            }
+            res.json(article)
+        })
+        .catch(next)
 })
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
+})
+
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: 'Server error' }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
 })
 
 module.exports = app
