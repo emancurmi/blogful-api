84fc19025f6fda32673b6de6dc8cc1b70f6e50ba
Eman Curmi <emancurmi@gmail.com>
2020-07-02 01:55:58 -0400

    updates

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const xss = require('xss')
 const ArticlesService = require('./articles-service')
 
 const articlesRouter = express.Router()
@@ -18,6 +19,15 @@ articlesRouter
         const { title, content, style } = req.body
         const newArticle = { title, content, style }
 
+        for (const [key, value] of Object.entries(newArticle)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body` }
+                })
+            }
+        }
+
+
         ArticlesService.insertArticle(
             req.app.get('db'),
             newArticle
@@ -33,16 +43,38 @@ articlesRouter
 
 articlesRouter
     .route('/:article_id')
-    .get((req, res, next) => {
-        const knexInstance = req.app.get('db')
-        ArticlesService.getById(knexInstance, req.params.article_id)
+    .all((req, res, next) => {
+        ArticlesService.getById(
+            req.app.get('db'),
+            req.params.article_id
+        )
             .then(article => {
                 if (!article) {
                     return res.status(404).json({
                         error: { message: `Article doesn't exist` }
                     })
                 }
-                res.json(article)
+                res.article = article // save the article for the next middleware
+                next() // don't forget to call next so the next middleware happens!
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json({
+            id: res.article.id,
+            style: res.article.style,
+            title: xss(res.article.title), // sanitize title
+            content: xss(res.article.content), // sanitize content
+            date_published: res.article.date_published,
+        })
+    })
+    .delete((req, res, next) => {
+        ArticlesService.deleteArticle(
+            req.app.get('db'),
+            req.params.article_id
+        )
+            .then(() => {
+                res.status(204).end()
             })
             .catch(next)
     })
--- src/articles/articles-service.js
+++ src/articles/articles-service.js
@@ -2,6 +2,7 @@ const ArticlesService = {
     getAllArticles(knex) {
         return knex.select('*').from('blogful_articles')
     },
+
     insertArticle(knex, newArticle) {
         return knex
             .insert(newArticle)
@@ -11,14 +12,17 @@ const ArticlesService = {
                 return rows[0]
             })
     },
+
     getById(knex, id) {
         return knex.from('blogful_articles').select('*').where('id', id).first()
     },
+
     deleteArticle(knex, id) {
         return knex('blogful_articles')
             .where({ id })
             .delete()
     },
+
     updateArticle(knex, id, newArticleFields) {
         return knex('blogful_articles')
             .where({ id })
